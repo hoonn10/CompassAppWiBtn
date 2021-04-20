f9b9a3dca7a287d25aa095f5c242939826a0c4e3
C-H Han <itq1@naver.com>
2021-04-20 11:45:30 +0900

    compass + map

--- app/src/main/java/com/androidapp/compassappwibtn/MainActivity.java
+++ app/src/main/java/com/androidapp/compassappwibtn/MainActivity.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnOpenCompass;
-    Intent compassActivity;
+    Button btnOpenCompass, btnMap;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +17,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnOpenCompass = findViewById(R.id.btnOpenCompassView);
+        btnMap = findViewById(R.id.btnMap);
+
         btnOpenCompass.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnOpenCompassView :
-                compassActivity = new Intent(this, CompassActivity.class);
-                startActivity(compassActivity); break;
+                intent = new Intent(this, CompassActivity.class);break;
+            case R.id.btnMap:
+                intent = new Intent(this, MapsActivity.class); break;
             default: break;
         }
+        startActivity(intent);
     }
 }
